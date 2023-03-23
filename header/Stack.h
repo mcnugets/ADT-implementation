d99e76027ca6905d148c578bae25363b9fc06394
sulla <sultanscarface@gmail.com>
2023-03-24 05:08:14 +0600

    stack template class complete

--- header/Stack.h
+++ header/Stack.h
@@ -1,7 +1,9 @@
-#pragma once
 #define T template <typename type>
+#define conc const noexcept
 #include <iostream>
-#include <vector>
+
+#ifndef stack_header
+#define stack_header
 
 namespace adt
 {
@@ -9,42 +11,74 @@ namespace adt
     T class stack
     {
     private:
+        // member variables
         size_t _size;
-        int capacity;
+        size_t _capacity;
         type *arr;
-        initializer_list<type> *init_list;
+
+        // typedef initialization for initializer list
+        typedef std::initializer_list<type> in_list;
 
     public:
+        // constructors
         explicit stack();
         explicit stack(const stack<type> &input);
         explicit stack(const size_t &size);
-        stack(const std::initializer_list<type> &init_list);
+        stack(const in_list &init_list);
         ~stack();
 
+        /// write-only cannot be used withc const
         // modifiers
         constexpr void push(const type &input);
         constexpr void pop();
-        inline constexpr type &top();
-        constexpr bool find(const type &input);
-        size_t &size();
 
-        //---------------operators
+        /// read-only can be const
+        // access
+        constexpr type &top() conc;
+        constexpr bool find(const type &input) conc;
+
+        // capacity
+        constexpr size_t size() conc;
+        constexpr size_t capacity() conc;
+        constexpr bool empty() conc;
+
+        // operators
         int &operator[](const size_t i);
         type &operator=(type &assign_to);
     };
 };
+#endif stack_header
 
 #pragma region constructors
-T adt::stack<type>::stack() : _size(0), capacity(1), arr(new type[capacity])
+T adt::stack<type>::stack() : _size(0), _capacity(1)
 {
+    arr = new type[_capacity];
 }
 
-T adt::stack<type>::stack(const size_t &size) : _size(size), capacity(size * 2) ss, arr(new type[capacity]) {}
-
-T adt::stack<type>::stack(const adt::stack<type> &input) : this(input) {}
-
-T adt::stack<type>::stack(const std::initializer_list<type> &init_list) : init_list(&init_list)
+// constructor that defines the size of the stack
+T adt::stack<type>::stack(const size_t &size) : _size(size), _capacity(size * 2)
 {
+    arr = new type[_capacity];
+}
+// copy constructor
+T adt::stack<type>::stack(const adt::stack<type> &input) : _size(input._size), _capacity(input._capacity)
+{
+    this->arr = new type[this->_capacity];
+    for (int i = 0; i < this->_size; i++)
+    {
+        this->arr[i] = input.arr[i];
+    }
+}
+
+// init list constructor
+T adt::stack<type>::stack(const in_list &init_list) : _size(init_list.size()), _capacity(_size * 2)
+{
+
+    this->arr = new type[this->_capacity];
+    for (int i = 0; i < this->_size; i++)
+    {
+        this->arr[i] = *init_list.begin() + i;
+    }
 }
 
 T adt::stack<type>::~stack()
@@ -57,11 +91,11 @@ T adt::stack<type>::~stack()
 //----modifiers
 T constexpr void adt::stack<type>::push(const type &input)
 {
-    if (this->_size == this->capacity)
+    if (this->_size == this->_capacity)
     {
 
-        capacity = capacity * 2;
-        type *new_alloc = new type[capacity];
+        _capacity = _capacity * 2;
+        type *new_alloc = new type[_capacity];
         for (int x = 0; x < this->_size; x++)
         {
             new_alloc[x] = this->arr[x];
@@ -78,19 +112,19 @@ T constexpr void adt::stack<type>::push(const type &input)
     }
     this->arr[0] = input;
 }
-
-T inline constexpr type &adt::stack<type>::top()
-{
-    return this->arr[0];
-}
-
 T constexpr void adt::stack<type>::pop()
 {
     this->arr[0].~type();
     this->_size--;
 }
 
-T constexpr bool adt::stack<type>::find(const type &input)
+// access
+T inline constexpr type &adt::stack<type>::top() conc
+{
+    return this->arr[0];
+}
+
+T constexpr bool adt::stack<type>::find(const type &input) conc
 {
     for (int x = 0; x < this->_size; x++)
     {
@@ -100,13 +134,25 @@ T constexpr bool adt::stack<type>::find(const type &input)
     return false;
 }
 
-//----
-T size_t &adt::stack<type>::size()
+// capacity
+T constexpr size_t adt::stack<type>::size() conc
 {
     return this->_size;
 }
+T constexpr size_t adt::stack<type>::capacity() conc
+{
+    return this->_capacity;
+}
+T constexpr bool adt::stack<type>::empty() conc
+{
+    if (this._size == 0)
+    {
+        return true;
+    }
+    return false;
+}
 
-//--operator
+// operator
 T int &adt::stack<type>::operator[](const std::size_t i)
 {
     return this->arr[i];
--- main.cpp
+++ main.cpp
@@ -1,10 +1,21 @@
 #include <iostream>
 #include "header/Stack.h"
+#include <vector>
 using namespace std;
 using namespace adt;
 
 int main()
 {
     stack<int> st = {1, 2, 3};
+
+    stack<int> st2(st);
+
+    int cunt = st2.top();
+    st2.pop();
+    size_t soup = st.size();
+
+    soup = 4;
+    cout << st.size() << endl;
+
     return 0;
 }
